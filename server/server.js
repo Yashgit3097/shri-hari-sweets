e074ec6e788450a6141dda49885db20df7256163
Yash <yashgithug907@gmail.com>
2026-08-05 21:57:30 +0530

    feat: initialize express server with socket.io support and auto-ping heartbeat

--- server/server.js
+++ server/server.js
@@ -15,7 +15,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 const server = http.createServer(app);
